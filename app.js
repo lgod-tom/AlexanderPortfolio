d7b9f406ddafee55c03b06277e70a3c9b60cc26c
lgod-tom <tomalexanderphoto@gmail.com>
2016-01-27 21:47:50 -0500

    Portfolio files
    all the files needed to run site

---
+++ app.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var person = prompt("Please enter your name", "Jericho");
+
+    if (person != null) {
+        document.getElementById("name").innerHTML =
+        "Hey guess what " + person + " ? IT DOESN'T MATTER WHAT YOUR NAME IS - The Rock";
+    }
+}
+
+/* Don't know how to style the output text
+  Realllllly hoping one of you laughs at the Rock quote */
